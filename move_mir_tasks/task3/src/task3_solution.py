7bc33b8ea666103c325c48ab1eceef65c3b33656
Daniela Sidjui <celianthetchana0@gmail.com>
2024-01-08 10:37:27 +0100

    solution of task3
    Co-authored-by: hciays <hciays@users.noreply.github.com>

--- move_mir_tasks/task3/src/task3_solution.py
+++ move_mir_tasks/task3/src/task3_solution.py
@@ -2,7 +2,7 @@
 
 import rospy
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Pose
 import math
 import copy
 import time
@@ -10,42 +10,38 @@ import time
 class Task3Node():
     def __init__(self, topic: str ="odometry"):
         rospy.init_node("Task3", anonymous=True)
-        self.rate = rospy.Rate(5)
-        self.state = 0
-        self.current_pose = Odometry()
-        self.final_pose = 0
-        self.speed = Twist()
+        self.rate = rospy.Rate(1)
+        self.initial_position = 0
+        self.indicator = 0
+        self.current_position = 0
+        self.pos_diff= -1
+        self.speed = Odometry()
         if topic.lower() == "odometry":
             self.publisher = rospy.Publisher("/mobile_base_controller/cmd_vel", Twist, queue_size=10)
             self.subscriber = rospy.Subscriber("/mobile_base_controller/odom", Odometry, self.callback)
 
     def callback(self, msg):
         rospy.loginfo("Current position: x = {0}, y = {1} and z = {2} \n".format(msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z))
+        if self.indicator == 0:
+            self.initial_position = float(msg.pose.pose.position.x)
+            self.indicator+=1
+           
+        self.current_position = float(msg.pose.pose.position.x)
     
     def run(self):
         while not rospy.is_shutdown():
-             #rospy.loginfo("Current position: x = {0}, y = {1} and z = {2} \n".format(self.current_pose.pose.pose.position.x, self.current_pose.pose.pose.position.y, self.current_pose.pose.pose.position.z))
-    
-            if self.state == 0 :
-                self.final_pose = self.current_pose.pose.pose.position.x + 1
-                print("Ici",self.final_pose , self.current_pose.pose.pose.position.x, flush=True)
-                self.state+=1
-            elif self.state == 1:
-                while self.final_pose > self.current_pose.pose.pose.position.x :
-                    print("Code herev 11111111111111111", self.final_pose - self.current_pose.pose.pose.position.x, flush=True)
-                    self.speed.linear.x = 0.2
-                    self.publisher.publish(self.speed)
-                    print ("The robot moves")
-                print("Code herev 2222222222222222", flush=True)
-                self.speed.linear.x = 0
-                self.publisher.publish(self.speed)
-                print ("The robot stops") 
-                time.sleep(1)
-                
-                self.state = 0
+            while self.current_position - self.initial_position < 1:
+                self.pos_diff = self.current_position - self.initial_position
+                self.speed.twist.twist.linear.x = 0.2
+                self.publisher.publish(self.speed.twist.twist)
+                rospy.loginfo ("The robot moves. Position before {0}, now {1}".format(self.initial_position, self.current_position))
+            self.speed.twist.twist.linear.x = 0.0
+            self.indicator = 0
+            self.publisher.publish(self.speed.twist.twist)
+            self.rate.sleep()
+            rospy.loginfo ("The robot stooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooops position difference {0}".format(self.pos_diff))
             
-            else :
-                pass    
+            
             
 def main(args=None):
     #rospy.init(args=args)
